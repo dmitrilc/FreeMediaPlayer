c95055284c1c6a7a4ff0696f9a7edc8a0a98792c
dmitrilc <dmitrilc@users.noreply.github.com>
2022-03-25 15:37:29 -0400

    upgraded gradle added test deps upgraded navigation version

---
+++ app/src/androidTest/java/com/dimitrilc/freemediaplayer/MainActivityTest.kt
@@ -0,0 +1,20 @@
+package com.dimitrilc.freemediaplayer
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class MainActivityTest {
+
+    @Test
+    fun onCreate() {
+    }
+
+    @Test
+    fun onPause() {
+    }
+
+    @Test
+    fun onResume() {
+    }
+}
