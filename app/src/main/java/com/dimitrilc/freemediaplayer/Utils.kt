ff5fb51fc2a2dc911b08aae111cbd71befe56ffa
dmitrilc <dmitrilc@users.noreply.github.com>
2022-03-29 18:56:08 -0400

    Fix typo

--- app/src/main/java/com/dimitrilc/freemediaplayer/Utils.kt
+++ app/src/main/java/com/dimitrilc/freemediaplayer/Utils.kt
@@ -2,7 +2,7 @@ package com.dimitrilc.freemediaplayer
 
 import android.os.Build
 
-private val TAG = "DB_DEBUG"
+private val TAG = "DEBUG"
 
 fun isSameOrAfterS() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
 fun isSameOrAfterQ() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
