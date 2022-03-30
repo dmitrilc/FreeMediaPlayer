422bb9e587cf709229e2464804b5f29fcd1de3fb
dmitrilc <dmitrilc@users.noreply.github.com>
2022-03-30 18:32:40 -0400

    added first instrumented test

--- app/src/androidTest/java/com/dimitrilc/freemediaplayer/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/dimitrilc/freemediaplayer/ExampleInstrumentedTest.kt
@@ -1,12 +1,32 @@
 package com.dimitrilc.freemediaplayer
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onData
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.dimitrilc.freemediaplayer.ui.MainActivity
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.dimitrilc.freemediaplayer.ui.adapter.FoldersFullAdapter
+import com.dimitrilc.freemediaplayer.ui.state.FoldersUiState
+import org.hamcrest.Matchers
+import org.hamcrest.Matchers.*
+import com.dimitrilc.freemediaplayer.ui.adapter.FoldersFullAdapter.FolderFullViewHolder
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +34,31 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class dimitrilcInstrumentedTest {
+class FmpInstrumentedTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.dimitrilc.freemediaplayer", appContext.packageName)
+    fun test_isRecyclerFoldersFullVisible_onAppLaunch(){
+        onView(withId(R.id.recycler_FoldersFull))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_isRecyclerFoldersRelativeVisible_onClick() {
+        onView(withId(R.id.recycler_FoldersFull))
+            .perform(
+                actionOnItemAtPosition<FolderFullViewHolder>(
+                    0,
+                    click()
+                ))
+
+        onView(allOf(
+                withId(R.id.recycler_audioFoldersRelative),
+                withEffectiveVisibility(Visibility.VISIBLE)
+        )).check(
+            matches(isDisplayed())
+        )
     }
 }
