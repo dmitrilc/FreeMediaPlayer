f6352adb269d3ebb4914e1307598464ea3562b10
dmitrilc <dmitrilc@users.noreply.github.com>
2022-04-11 18:16:28 -0400

    made FoldersUiState.kt parcelable. This is needed for FoldersViewModel.kt to save ui state with SavedStateHandle.

--- app/src/main/java/com/dimitrilc/freemediaplayer/ui/state/FoldersUiState.kt
+++ app/src/main/java/com/dimitrilc/freemediaplayer/ui/state/FoldersUiState.kt
@@ -1,7 +1,11 @@
 package com.dimitrilc.freemediaplayer.ui.state
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class FoldersUiState(
     val parentPath: String,
     val isExpanded: Boolean = false,
     val relativePaths: List<String>
-)
+) : Parcelable
