a45c881b7a5ccb3d3d2b380f3ea53fc119bb7771
dmitrilc <dmitrilc@users.noreply.github.com>
2022-05-16 13:13:37 -0400

    Enabled databinding changed implementation to ListAdapter hopefully animations will be smoother onMoved and onSwiped

--- app/src/main/java/com/dimitrilc/freemediaplayer/ui/adapter/ActivePlaylistItemAdapter.kt
+++ app/src/main/java/com/dimitrilc/freemediaplayer/ui/adapter/ActivePlaylistItemAdapter.kt
@@ -2,14 +2,69 @@ package com.dimitrilc.freemediaplayer.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.fragment.app.findFragment
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.dimitrilc.freemediaplayer.R
 import com.dimitrilc.freemediaplayer.databinding.ActivePlaylistItemViewBinding
-import com.dimitrilc.freemediaplayer.data.entities.MediaItem
-import com.dimitrilc.freemediaplayer.ui.fragments.ActivePlaylistFragment
+import com.dimitrilc.freemediaplayer.ui.state.folders.items.FolderItemsUiState
 
 private const val TAG = "ACTIVE_PLAYLIST_ITEM_ADAPTER"
 
+class ActivePlaylistItemAdapter(private val dataSet: List<FolderItemsUiState>) :
+    ListAdapter<FolderItemsUiState, ActivePlaylistItemAdapter.ActivePlaylistItemViewHolder>(DIFF_CALLBACK) {
+
+    class ActivePlaylistItemViewHolder(val activePlaylistItemViewBinding: ActivePlaylistItemViewBinding) :
+        RecyclerView.ViewHolder(activePlaylistItemViewBinding.root)
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ActivePlaylistItemViewHolder {
+        val activePlaylistItemViewBinding = ActivePlaylistItemViewBinding.inflate(
+            LayoutInflater.from(viewGroup.context),
+            viewGroup,
+            false)
+
+        return ActivePlaylistItemViewHolder(activePlaylistItemViewBinding)
+    }
+
+    override fun onBindViewHolder(viewHolder: ActivePlaylistItemViewHolder, position: Int) {
+        val item = dataSet[position]
+        viewHolder.activePlaylistItemViewBinding.adapterPos = position
+        viewHolder.activePlaylistItemViewBinding.state = item
+
+        val thumb = item.thumbnailLoader(item.thumbnailUri, item.videoId)
+        if (thumb != null){
+            viewHolder.activePlaylistItemViewBinding
+                .imageViewActivePlaylistItemDisplayArt
+                .setImageBitmap(thumb)
+        } else {
+            viewHolder.activePlaylistItemViewBinding
+                .imageViewActivePlaylistItemDisplayArt
+                .setImageResource(R.drawable.ic_baseline_music_note_24)
+        }
+    }
+
+    override fun getItemCount() = dataSet.size
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<FolderItemsUiState>() {
+            override fun areItemsTheSame(
+                oldItem: FolderItemsUiState,
+                newItem: FolderItemsUiState
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: FolderItemsUiState,
+                newItem: FolderItemsUiState
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
+
+/*
 class ActivePlaylistItemAdapter(private val dataSet: MutableList<MediaItem>) :
     RecyclerView.Adapter<ActivePlaylistItemAdapter.ActivePlaylistItemViewHolder>() {
 
@@ -50,8 +105,10 @@ class ActivePlaylistItemAdapter(private val dataSet: MutableList<MediaItem>) :
 
         val item = dataSet[holder.bindingAdapterPosition]
 
-        holder.displayArtView.findFragment<ActivePlaylistFragment>()
-            .onAdapterChildThumbnailLoad(holder.displayArtView, item.albumArtUri!!, item.mediaItemId)
+*/
+/*        holder.displayArtView.findFragment<ActivePlaylistFragment>()
+            .onAdapterChildThumbnailLoad(holder.displayArtView, item.albumArtUri!!, item.mediaItemId)*//*
+
     }
 
-}
+}*/
